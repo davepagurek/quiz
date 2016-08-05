fd5e9c62f56ae5b5e51742bbdd2d179d4a5d9f1a
Andrew R. McBurney <mcburney.andrew13@gmail.com>
2016-08-05 18:17:25 -0400

    Updated Score object.

--- quiz/Score.h
+++ quiz/Score.h
@@ -16,6 +16,11 @@ friend std::ostream& operator<< (std::ostream& sout, const Score& s) {
 
 public:
 	Score(int m) : score_(0), max_(m) {}
+
+	void reset () {
+		score_ = 0;
+		max_ = 0;
+	}
 	void increment_max () {
 		max_ ++;
 	}
